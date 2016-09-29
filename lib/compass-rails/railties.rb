5a69a9857515deb3f47a31f1b6ae3877cd1e2d38
Robert Dilio <robert@relatednoise.com>
2016-09-29 16:10:25 -0400

    removing rails 5 warning

--- lib/compass-rails/railties.rb
+++ lib/compass-rails/railties.rb
@@ -1,9 +1,11 @@
 if defined?(::Rails)
-  if CompassRails.rails31? || CompassRails.rails32?
-    require "compass-rails/railties/3_1"
-  elsif CompassRails.rails4?
-    require "compass-rails/railties/4_0"
-  else
-    $stderr.puts "Unsupported rails environment for compass"
-  end
+	if CompassRails.rails31? || CompassRails.rails32?
+		require "compass-rails/railties/3_1"
+	elsif CompassRails.rails4?
+		require "compass-rails/railties/4_0"
+	elsif CompassRails.rails5?
+		
+	else
+		$stderr.puts "Unsupported rails environment for compass"
+	end
 end
